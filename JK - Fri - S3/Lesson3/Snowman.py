ecf29a4fd0e3f102ca75a1d0698a1c53eca86c48
Ahmad Yazid <62284260+madyazdhil@users.noreply.github.com>
2023-10-14 10:32:35 +0800

    from KN's Laptop

--- JK - Fri - S3/Lesson3/Snowman.py
+++ JK - Fri - S3/Lesson3/Snowman.py
@@ -13,15 +13,20 @@ arcade.set_background_color(arcade.color.SKY_BLUE)
 arcade.start_render()
 
 # draw the snowman
-arcade.draw_circle_filled(300, 450, 60, arcade.color.WHITE)  # bottom circle
-arcade.draw_circle_filled(300, 550, 80, arcade.color.WHITE)  # middle circle
-arcade.draw_circle_filled(300, 625, 40, arcade.color.WHITE)  # top circle
+arcade.draw_circle_filled(300, 350, 60, arcade.color.WHITE)  # bottom circle
+arcade.draw_circle_filled(300, 250, 80, arcade.color.WHITE)  # middle circle
+arcade.draw_circle_filled(300, 435, 40, arcade.color.WHITE)  # top circle
 
 # draw the stick hand
 arcade.draw_line(330, 575, 360, 575, arcade.color.BLACK, 4)
 
 # draw the nose
 arcade.draw_triangle_filled(300, 625, 290, 605, 310, 605, arcade.color.ORANGE)
+
+arcade.draw_triangle_filled(300,440,
+                            300,430,
+                            370,435,
+                            arcade.color.ORANGE_PEEL)
 
 #finishing rendering
 arcade.finish_render()
